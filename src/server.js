466420f0ab5cf51ff7eb1dfb4fe2a69cebbb75c1
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-07-10 11:58:17 +0530

    added socket.io boilerplate

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server); // socketio requires raw http server.
+
 const PORT = process.env.PORT || 3000;
 
-const publicDirPath = path.join(__dirname, "../public");
-server.use(express.static(publicDirPath));
+const publicDirPath = path.join(__dirname, "./public");
+app.use(express.static(publicDirPath));
+
+io.on("connection", () => {
+	console.log("New web socket connection");
+});
 
 server.listen(PORT, () => console.log("Server running on port :", PORT));
