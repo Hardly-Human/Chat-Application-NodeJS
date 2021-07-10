252b50cfcc49f03fcf5f86b3406e28bc22a09317
Shaik Rehanuddin <rehanuddin1024@gmail.com>
2021-07-10 12:44:09 +0530

    Added count updater with socketio

--- src/public/JS/chat.js
+++ src/public/JS/chat.js
@@ -1,1 +1,11 @@
-io();
+const socket = io();
+
+const message = document.getElementById("message");
+
+socket.on("countUpdated", (count) => {
+	message.innerText = `Count : ${count}`;
+});
+
+document.querySelector("#increment").addEventListener("click", () => {
+	socket.emit("increment");
+});
--- src/server.js
+++ src/server.js
@@ -12,8 +12,20 @@ const PORT = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, "./public");
 app.use(express.static(publicDirPath));
 
-io.on("connection", () => {
+let count = 0;
+
+// server [emit] => client [recieve] - countUpdated
+// client [emit] => server [recieve] - increment
+
+io.on("connection", (socket) => {
 	console.log("New web socket connection");
+
+	// socket.emit("countUpdated", count);
+
+	socket.on("increment", () => {
+		count += 1;
+		io.emit("countUpdated", count);
+	});
 });
 
 server.listen(PORT, () => console.log("Server running on port :", PORT));
